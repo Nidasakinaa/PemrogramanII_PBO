d772f7bb15d44d337b4ea39ee5153d89267ac8cc
Nidasakinaa <nidasakinaaulia@gmail.com>
2023-11-07 20:16:58 +0700

    Tugas Pertemuan 6

---
+++ Pertemuan06/Praktikum/P6_4_714220040(1)/P6_4_714220040(1)/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace P6_4_714220040_1_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbAlamat = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.cbAgama = new System.Windows.Forms.ComboBox();
+            this.tbNoTelp = new System.Windows.Forms.TextBox();
+            this.btnDaftar = new System.Windows.Forms.Button();
+            this.btnSelesai = new System.Windows.Forms.Button();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.epWarning = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epError = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epCorrect = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epError)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(126, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(664, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FORM PENDAFTARAN SISWA BARU TK BUNGA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Alamat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tanggal Lahir :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 346);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(195, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "No. Telp Ortu / Wali :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Jenis Kelamin :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(48, 297);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Agama";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(240, 93);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(385, 29);
+            this.tbName.TabIndex = 7;
+            this.tbName.TextChanged += new System.EventHandler(this.tbName_Leave);
+            // 
+            // tbAlamat
+            // 
+            this.tbAlamat.Location = new System.Drawing.Point(240, 193);
+            this.tbAlamat.Name = "tbAlamat";
+            this.tbAlamat.Size = new System.Drawing.Size(385, 29);
+            this.tbAlamat.TabIndex = 8;
+            this.tbAlamat.TextChanged += new System.EventHandler(this.tbAlamat_Leave);
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(240, 142);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(385, 29);
+            this.dateTimePicker.TabIndex = 9;
+            // 
+            // cbAgama
+            // 
+            this.cbAgama.FormattingEnabled = true;
+            this.cbAgama.Items.AddRange(new object[] {
+            "Islam",
+            "Kristen",
+            "Khatolik",
+            "Hindu",
+            "Buddha",
+            "Konghucu"});
+            this.cbAgama.Location = new System.Drawing.Point(240, 291);
+            this.cbAgama.Name = "cbAgama";
+            this.cbAgama.Size = new System.Drawing.Size(385, 32);
+            this.cbAgama.TabIndex = 11;
+            // 
+            // tbNoTelp
+            // 
+            this.tbNoTelp.Location = new System.Drawing.Point(240, 342);
+            this.tbNoTelp.Name = "tbNoTelp";
+            this.tbNoTelp.Size = new System.Drawing.Size(385, 29);
+            this.tbNoTelp.TabIndex = 12;
+            this.tbNoTelp.TextChanged += new System.EventHandler(this.tbNoTelp_TextChanged);
+            // 
+            // btnDaftar
+            // 
+            this.btnDaftar.Location = new System.Drawing.Point(46, 505);
+            this.btnDaftar.Name = "btnDaftar";
+            this.btnDaftar.Size = new System.Drawing.Size(159, 62);
+            this.btnDaftar.TabIndex = 13;
+            this.btnDaftar.Text = "Daftar";
+            this.btnDaftar.UseVisualStyleBackColor = true;
+            this.btnDaftar.Click += new System.EventHandler(this.btnDaftar_Click);
+            // 
+            // btnSelesai
+            // 
+            this.btnSelesai.Location = new System.Drawing.Point(660, 505);
+            this.btnSelesai.Name = "btnSelesai";
+            this.btnSelesai.Size = new System.Drawing.Size(161, 62);
+            this.btnSelesai.TabIndex = 14;
+            this.btnSelesai.Text = "Selesai";
+            this.btnSelesai.UseVisualStyleBackColor = true;
+            this.btnSelesai.Click += new System.EventHandler(this.btnSelesai_Click);
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(240, 240);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(138, 29);
+            this.rb1.TabIndex = 15;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "Perempuan";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(443, 240);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(109, 29);
+            this.rb2.TabIndex = 16;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "Laki-laki";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // epWarning
+            // 
+            this.epWarning.ContainerControl = this;
+            this.epWarning.Icon = ((System.Drawing.Icon)(resources.GetObject("epWarning.Icon")));
+            // 
+            // epError
+            // 
+            this.epError.ContainerControl = this;
+            this.epError.Icon = ((System.Drawing.Icon)(resources.GetObject("epError.Icon")));
+            // 
+            // epCorrect
+            // 
+            this.epCorrect.ContainerControl = this;
+            this.epCorrect.Icon = ((System.Drawing.Icon)(resources.GetObject("epCorrect.Icon")));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 609);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
+            this.Controls.Add(this.btnSelesai);
+            this.Controls.Add(this.btnDaftar);
+            this.Controls.Add(this.tbNoTelp);
+            this.Controls.Add(this.cbAgama);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.tbAlamat);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form Pendaftaran Siswa Baru";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epError)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbAlamat;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.ComboBox cbAgama;
+        private System.Windows.Forms.TextBox tbNoTelp;
+        private System.Windows.Forms.Button btnDaftar;
+        private System.Windows.Forms.Button btnSelesai;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.ErrorProvider epWarning;
+        private System.Windows.Forms.ErrorProvider epError;
+        private System.Windows.Forms.ErrorProvider epCorrect;
+    }
+}
+
---
+++ Pertemuan06/Praktikum/P6_4_714220040(1)/P6_4_714220040(1)/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P6_4_714220040_1_
+{
+    public partial class Form1 : Form
+    {
+        private string jkelamin;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbName_Leave(object sender, EventArgs e)
+        {
+            string inputName = tbName.Text;
+
+            if (string.IsNullOrEmpty(inputName))
+            {
+                epWarning.SetError(tbName, "Nama Tidak Boleh Kosong!");
+                epError.SetError(tbName, "");
+                epCorrect.SetError(tbName, "");
+            }
+            else
+            {
+                if (char.IsUpper(inputName[0]) && inputName.Substring(1).All(char.IsLetter))
+                {
+                    epWarning.SetError(tbName, "");
+                    epError.SetError(tbName, "");
+                    epCorrect.SetError(tbName, "Benar!");
+                }
+                else
+                {
+                    epWarning.SetError(tbName, "");
+                    epError.SetError(tbName, "Awali dengan Huruf Kapital!");
+                    epCorrect.SetError(tbName, "");
+                }
+            }
+        }
+
+        private void tbAlamat_Leave(object sender, EventArgs e)
+        {
+            string inputAlamat = tbAlamat.Text;
+
+            if (string.IsNullOrEmpty(inputAlamat))
+            {
+                epWarning.SetError(tbAlamat, "Alamat Tidak Boleh Kosong!");
+                epError.SetError(tbAlamat, "");
+                epCorrect.SetError(tbAlamat, "");
+            }
+            else
+            {
+                if (char.IsLower(inputAlamat[0]) && inputAlamat.Substring(1).All(char.IsLetter))
+                {
+                    epWarning.SetError(tbAlamat, "");
+                    epError.SetError(tbAlamat, "");
+                    epCorrect.SetError(tbAlamat, "Benar!");
+                }
+                else
+                {
+                    epWarning.SetError(tbAlamat, "");
+                    epError.SetError(tbAlamat, "Awali dengan Huruf Kecil!");
+                    epCorrect.SetError(tbAlamat, "");
+                }
+            }
+        }
+
+        private void btnDaftar_Click(object sender, EventArgs e)
+        {
+            String jkelamin = "";
+            String agama = "";
+
+            if (rb1.Checked == true)
+            {
+                jkelamin = "Perempuan";
+            }
+            else if (rb2.Checked == true)
+            {
+                jkelamin = "Laki-laki";
+            }
+            else
+            {
+                MessageBox.Show("Pilih Jenis Kelamin!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (cbAgama.Text != "Islam" && cbAgama.Text != "Kristen" && cbAgama.Text != "Katholik" && cbAgama.Text != "Hindu" && cbAgama.Text != "Buddha" && cbAgama.Text != "Konghucu")
+            {
+                MessageBox.Show("Pilih Agama!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            MessageBox.Show("Nama : " + tbName.Text +
+              "\nTanggal Lahir :" + dateTimePicker.Text +
+              "\nAlamat :" + tbAlamat.Text +
+              "\nJenis Kelamin :" + jkelamin +
+              "\nAgama :" + cbAgama.Text +
+              "\nNo Telp :" + tbNoTelp.Text,
+              "Informasi Pendaftaran",
+              MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void tbNoTelp_TextChanged(object sender, EventArgs e)
+        {
+            if (tbNoTelp.Text == "")
+            {
+                epWarning.SetError(tbNoTelp, "No Telp Harus Diisi!");
+                epError.SetError(tbNoTelp, "");
+                epCorrect.SetError(tbNoTelp, "");
+            }
+            else
+            {
+                if ((tbNoTelp.Text).All(char.IsNumber))
+                {
+                    if (tbNoTelp.Text.Length >= 10 && tbNoTelp.Text.Length <= 13)
+                    {
+                        epWarning.SetError(tbNoTelp, "");
+                        epError.SetError(tbNoTelp, "");
+                        epCorrect.SetError(tbNoTelp, "Betul!");
+                    }
+                    else
+                    {
+                        epWarning.SetError(tbNoTelp, "No Telp Minimal 10 angka!");
+                        epError.SetError(tbNoTelp, "");
+                        epCorrect.SetError(tbNoTelp, "");
+                    }
+                }
+                else
+                {
+                    epWarning.SetError(tbNoTelp, "");
+                    epError.SetError(tbNoTelp, "Hanya Angka yang Dapat Diinputkan!");
+                    epCorrect.SetError(tbNoTelp, "");
+                }
+            }
+        }
+
+
+        private void btnSelesai_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
